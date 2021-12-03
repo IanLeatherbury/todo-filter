3f25fea9729791a6b7300fccf7a22e05ac05174c
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-12-02 19:30:30 -0800

    clear out old todo items

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { CraftBlock, CraftTextBlock } from "@craftdocs/craft-extension-api";
 import * as React from "react"
 import * as ReactDOM from 'react-dom'
 import craftXIconSrc from "./craftx-icon.png"
@@ -19,11 +20,16 @@ const App: React.FC<{}> = () => {
     alignItems: "center",
   }}>
     <img className="icon" src={craftXIconSrc} alt="CraftX logo" />
-    <button className={`btn ${isDarkMode ? "dark" : ""}`} onClick={insertHelloWorld}>
-      Hello world!!!
+    <button className={`btn ${isDarkMode ? "dark" : ""}`} onClick={copyTheCurrentPage}>
+      Copy page
+    </button>
+    <button className={`btn ${isDarkMode ? "dark" : ""}`} onClick={paste}>
+      Paste page
     </button>
   </div>;
 }
+
+var loadedBlocks: CraftBlock[] = []
 
 function useCraftDarkMode() {
   const [isDarkMode, setIsDarkMode] = React.useState(false);
@@ -41,6 +47,82 @@ function insertHelloWorld() {
   });
 
   craft.dataApi.addBlocks([block]);
+}
+
+async function copyTheCurrentPage() {
+  // Query the page which is currently opened
+  const result = await craft.dataApi.getCurrentPage()
+
+  // Check if the query succeeded
+  if (result.status !== "success") {
+    throw new Error(result.message)
+  }
+
+  const pageBlock = result.data
+  // Save the data into a global variable
+
+  var blocksToKeep: CraftBlock[] = []
+
+  pageBlock.subblocks.filter(async block => {
+    if (block.listStyle.type === "todo" && block.listStyle.state === "checked") {
+      return
+    } else {
+      blocksToKeep.push(block)
+    }
+  })
+
+  loadedBlocks = blocksToKeep
+}
+
+async function selectDoneItems() {
+  // Query the current page
+  const result = await craft.dataApi.getCurrentPage();
+
+  // Check for errors
+  if (result.status != "success") {
+    throw new Error(result.message)
+  }
+
+  // Look for blocks which contain the word
+  const blocks = result.data.subblocks.filter(block => {
+    // Only todo blocks are relevant
+    if (block.listStyle.type !== "todo") {
+      return false
+    }
+
+    // Merge the text run
+    const content = block.content.map(x => x.text).join()
+
+    // Search for the word
+    const hasLorem = content.indexOf("lorem") > -1
+
+    return hasLorem
+  })
+
+  // Extract the block ids
+  const blockIds = blocks.map(x => x.id)
+
+  // Make those blocks selected
+  const selectResult = await craft.editorApi.selectBlocks(blockIds)
+
+  // Check for errors
+  if (selectResult.status != "success") {
+    throw new Error(selectResult.message)
+  }
+
+  // Optionally we can check which blocks were selected
+  const selectedBlocks = selectResult.data
+
+}
+
+async function paste() {
+  const blocks = loadedBlocks
+  const result = await craft.dataApi.addBlocks(blocks)
+
+  // Check if the update succeeded
+  if (result.status !== "success") {
+    throw new Error(result.message)
+  }
 }
 
 export function initApp() {

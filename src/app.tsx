97ae8106dc8a4ce6bb9cb5376d9883b17734abfe
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-12-05 10:16:15 -0800

    fix bag and add incomplete

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,67 @@ import * as ReactDOM from 'react-dom'
 import './style.css'
 import 'tailwindcss/tailwind.css'
 import { CheckCircleIcon } from '@heroicons/react/solid'
-
-
+import { useState } from "react";
 
 const App: React.FC<{}> = () => {
   const isDarkMode = useCraftDarkMode();
+
+  const [incompleteOnly, setIncompleteOnly] = useState(false);
+
+  async function cutAndFilterTheCurrentPage() {
+    // Query the page which is currently opened
+    const result = await craft.dataApi.getCurrentPage()
+
+    // Check if the query succeeded
+    if (result.status !== "success") {
+      throw new Error(result.message)
+    }
+
+    const pageBlock = result.data
+    // Save the data into a global variable
+
+    var uncheckedBlocks: CraftBlock[] = []
+    var uncheckedBlockIds: string[] = []
+    var uncheckedOnly: CraftBlock[] = []
+    var uncheckedOnlyIds: string[] = []
+
+    pageBlock.subblocks.filter(async block => {
+      if (incompleteOnly === true) {
+        if (block.listStyle.type === "todo" && block.listStyle.state === "unchecked") {
+          uncheckedOnly.push(block)
+          uncheckedOnlyIds.push(block.id)
+        } else {
+          return
+        }
+        loadedBlocks = uncheckedOnly
+        await craft.dataApi.deleteBlocks(uncheckedOnlyIds)
+      } else {
+        if (block.listStyle.type === "todo" && block.listStyle.state === "checked") {
+          return
+        } else {
+          uncheckedBlockIds.push(block.id)
+          uncheckedBlocks.push(block)
+        }
+        await craft.dataApi.deleteBlocks(uncheckedBlockIds)
+        loadedBlocks = uncheckedBlocks
+      }
+    })
+  }
+
+
+  async function paste() {
+    const blocks = loadedBlocks
+    const result = await craft.dataApi.addBlocks(blocks)
+
+    // Check if the update succeeded
+    if (result.status !== "success") {
+      throw new Error(result.message)
+    }
+  }
+
+  async function handleincompleteChange() {
+    setIncompleteOnly(!incompleteOnly)
+  }
 
   React.useEffect(() => {
     if (isDarkMode) {
@@ -24,12 +80,33 @@ const App: React.FC<{}> = () => {
         <div className="flex justify-center my-2">
           <CheckCircleIcon className="w-10 h-10 text-blue-500" />
         </div>
-        <button className={`btn ${isDarkMode ? "dark" : "px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"}`} onClick={copyAndFilterTheCurrentPage}>
+        <button className={`btn ${isDarkMode ? "dark" : "px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"}`} onClick={cutAndFilterTheCurrentPage}>
           Cut and filter
         </button>
         <button className={`btn ${isDarkMode ? "dark" : "mt-2 px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"}`} onClick={paste}>
           Paste
         </button>
+        {/* Incomplete Settings */}
+        <div className="relative flex items-start my-4">
+          <div className="flex items-center h-5">
+            <input
+              id="incomplete"
+              name="incomplete"
+              type="checkbox"
+              checked={incompleteOnly}
+              onChange={handleincompleteChange}
+              className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+            />
+          </div>
+          <div className="ml-3 text-sm">
+            <label htmlFor="incomplete" className="font-medium text-gray-700">
+              Incomplete Only
+            </label>
+            <p id="incomplete-description" className="text-gray-500">
+              Only cut incomplete todos.
+            </p>
+          </div>
+        </div>
       </div>
     </div>);
 }
@@ -46,42 +123,6 @@ function useCraftDarkMode() {
   return isDarkMode;
 }
 
-async function copyAndFilterTheCurrentPage() {
-  // Query the page which is currently opened
-  const result = await craft.dataApi.getCurrentPage()
-
-  // Check if the query succeeded
-  if (result.status !== "success") {
-    throw new Error(result.message)
-  }
-
-  const pageBlock = result.data
-  // Save the data into a global variable
-
-  var blocksToKeep: CraftBlock[] = []
-  var blocksToDelete: string[] = []
-
-  pageBlock.subblocks.filter(async block => {
-    if (block.listStyle.type === "todo" && block.listStyle.state === "checked") {
-      blocksToDelete.push(block.id)
-    } else {
-      blocksToKeep.push(block)
-    }
-  })
-
-  await craft.dataApi.deleteBlocks(blocksToDelete)
-  loadedBlocks = blocksToKeep
-}
-
-async function paste() {
-  const blocks = loadedBlocks
-  const result = await craft.dataApi.addBlocks(blocks)
-
-  // Check if the update succeeded
-  if (result.status !== "success") {
-    throw new Error(result.message)
-  }
-}
 
 export function initApp() {
   ReactDOM.render(<App />, document.getElementById('react-root'))
